258dcf1a3561c54cce2cac3b02fa5cb626a30322
Vitaliy <vitaliy.fry@gmail.com>
2016-07-14 02:04:05 +0300

    fixes

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -9,5 +9,6 @@ class CitiesController < ApplicationController
   def show
     @city = City.find(params[:id])
     @coordinates = Map::GetCoordinatsByAddressService.new(@city.name).call
+    @markers = [['Bondi Beach', -33.890542, 151.274856],['Coogee Beach', -33.923036, 151.259052],['Cronulla Beach', -34.028249, 151.157507],['Manly Beach', -33.80010128657071, 151.28747820854187],['Maroubra Beach', -33.950198, 151.259302]]
   end
 end
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,5 @@
 class City < ActiveRecord::Base
-  
+
   belongs_to :country
   belongs_to :region
 
