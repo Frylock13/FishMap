23559bb4db87005297d49abd0ca45dcd919ca8b3
Vitaliy <vitaliy.fry@gmail.com>
2016-07-23 07:02:30 +0300

    fix marker on place page

--- app/models/category.rb
+++ app/models/category.rb
@@ -6,4 +6,8 @@ class Category < ActiveRecord::Base
 
   has_attached_file :marker_image, styles: { small: "25x25" }, default_url: "places/missing.png"
   validates_attachment_content_type :marker_image, content_type: /\Aimage\/.*\Z/
+
+  def marker_image_url
+    ActionController::Base.helpers.image_url(self.marker_image.url(:small))
+  end
 end
--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -3,6 +3,6 @@ class CategorySerializer < ActiveModel::Serializer
   attributes :marker_image_url
 
   def marker_image_url
-    ActionController::Base.helpers.image_url(object.marker_image.url(:small))
+    object.marker_image_url
   end
 end
