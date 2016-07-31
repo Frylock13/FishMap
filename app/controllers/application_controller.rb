4fec7c02da4828023cf2f132bf67947d0b5c67f8
Vitaliy <vitaliy.fry@gmail.com>
2016-07-31 03:12:38 +0300

    fix header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :get_custom_page_slugs
 
   def get_current_coordinates
     @latitude  = request.location.latitude
     @longitude = request.location.longitude
+  end
+
+  def get_custom_page_slugs
+    @custom_pages = Page.active
   end
 end
