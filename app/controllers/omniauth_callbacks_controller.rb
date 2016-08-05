81df09f60be5d8a017650458ca62e60bb795dc96
Vitaliy <vitaliy.fry@gmail.com>
2016-08-05 13:21:41 +0300

    fix soc

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,14 +1,24 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in(@user)
-    redirect_to root_path
+    begin
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+      sign_in(@user)
+      redirect_to root_path
+    rescue ActiveRecord::RecordNotUnique
+      redirect_to :back
+      flash[:danger] = 'Вы уже зарегистрированы через другую социальную сеть'
+    end
   end
 
   def vkontakte
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    sign_in(@user)
-    redirect_to root_path
+    begin
+      @user = User.from_omniauth(request.env["omniauth.auth"])
+      sign_in(@user)
+      redirect_to root_path
+    rescue ActiveRecord::RecordNotUnique
+      redirect_to :back
+      flash[:danger] = 'Вы уже зарегистрированы через другую социальную сеть'
+    end
   end
 end
