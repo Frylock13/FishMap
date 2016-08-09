422d654db6b1a36ca2f57fc7c83ad8d588344ab4
Vitaliy <vitaliy.fry@gmail.com>
2016-08-09 03:35:03 +0300

    reviews count on card

--- app/serializers/place_serializer.rb
+++ app/serializers/place_serializer.rb
@@ -1,10 +1,14 @@
 class PlaceSerializer < ActiveModel::Serializer
 
-  attributes :id, :title, :description, :latitude, :longitude, :reviews_count, :rating, :rating_image_url
+  attributes :id, :title, :description, :latitude, :longitude, :reviews_count, :rating, :rating_image_url, :full_reviews_count
 
   belongs_to :category
 
   def rating_image_url
     ActionController::Base.helpers.image_url("ratings/#{object.rating.round}.png")
+  end
+
+  def full_reviews_count
+    object.reviews.with_comments.count
   end
 end
